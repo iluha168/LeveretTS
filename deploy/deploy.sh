7f1fca4286eb009db0f21c75ca9ce73c005aa7f2
iluha168 <illya.chernyshov888@gmail.com>
2025-01-21 15:15:19 +0700

    fix deployment edge cases
    such as newline

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -5,7 +5,6 @@ deno check -q $1 &&\
 deno run -WRE npm:typescript/tsc --noResolve --noCheck --module commonjs --lib esnext --target esnext --outDir dist $1 &&\
 deno run -A https://deno.land/x/esbuild/mod.js dist/* --minify --outfile=$OUT_PATH --allow-overwrite 2> /dev/null || exit 1
 
-COMPILED=$(cat $OUT_PATH)
-echo ${COMPILED:0:-10} | deno run -NER deploy/index.mts $(basename -- "$1" ".${1#*.}")
+deno run -NER deploy/index.mts $(basename -- "$1" ".${1#*.}") $OUT_PATH
 
 rm -rf dist
