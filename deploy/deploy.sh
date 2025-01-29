6e718009328b1c67d5c67394fc966f259cd0a8e3
iluha168 <illya.chernyshov888@gmail.com>
2025-01-29 19:24:46 +0700

    fix minor deployment issues

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 OUT_PATH=dist/o.js
 
-deno check -q $1 &&\
-deno run -WRE npm:typescript/tsc --noResolve --noCheck --module commonjs --lib esnext --target esnext --outDir dist $1 &&\
-deno run -A https://deno.land/x/esbuild/mod.js dist/* --minify --outfile=$OUT_PATH --allow-overwrite 2> /dev/null || exit 1
+set -e
+
+deno check -q $1
+rm -rf dist
+deno run -WRE npm:typescript/tsc --noResolve --noCheck --module commonjs --lib esnext --target esnext --outDir dist $1
+deno run -A https://deno.land/x/esbuild/mod.js dist/* --minify --outfile=$OUT_PATH --allow-overwrite 2> /dev/null
 
 deno run -NER deploy/index.mts $(basename -- "$1" ".${1#*.}") $OUT_PATH
-
-rm -rf dist
